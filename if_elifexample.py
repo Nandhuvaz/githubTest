2dad4d650ac9858d6c38d10915c0af35d138860c
Nandhuvaz <137207752+Nandhuvaz@users.noreply.github.com>
2024-10-29 13:29:12 +0400

    Add files via upload

---
+++ if_elifexample.py
@@ -0,0 +1,12 @@
+name ='bob'
+age = 3000
+if name =='Alice':
+    print ('Hi Alice') 
+elif age <12:
+        print ('you are not alice kiddo.')
+elif age >2000:
+        print('unlike you. ALice is not an undead, immortal vampire.')
+elif age >100:
+        print ('you are not Alice,grannie.')
+        
+ 
